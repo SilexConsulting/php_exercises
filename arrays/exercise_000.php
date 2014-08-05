755cfdcf856631ca23a8e1e4c80d0ed4d69d27d4
David Otton <david@otton.org>
2014-08-05 10:55:04 +0100

    fix exercise 0

--- arrays/exercise_000.php
+++ arrays/exercise_000.php
@@ -4,10 +4,10 @@
  * Insert the numbers [1:50], inclusive, in $integers.
  */
 
-$integers = ?;
+$integers = array();
 
 /**
  * Insert the EVEN numbers between 1 and 50 in $even_integers.
  */
 
-$even_integers = ?;
+$even_integers = array();
