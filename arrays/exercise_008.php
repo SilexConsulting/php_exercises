1fd0ffbc0c7c421a5f4153bd23381ed5b0e86c3f
David Otton <david@otton.org>
2014-08-06 10:58:39 +0100

    Clarify 008

--- arrays/exercise_008.php
+++ arrays/exercise_008.php
@@ -3,11 +3,15 @@
 $a1 = array(1,2,3,4,5,6,7,8,9,10);
 $a2 = array(6,7,8,9,10,11,12);
 
+var_dump(my_array_intersection($a1, $a2));
+
 /**
- * Give me the INTERSECTION of a1 and a2. That is, all the elements that are in A1 AND A2.
+ * Write a function that accepts two arrays and returns their INTERSECTION as an array. That is, only the elements that are in A1 AND A2.
  *
  * Avoid array_*() functions.
  */
 
-$a3 = ?;
+function my_array_intersection($array1, $array2)
+{
 
+}
