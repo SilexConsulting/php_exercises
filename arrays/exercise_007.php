5b4a9997d05f4fb154190c691f8bec70e6956972
David Otton <david@otton.org>
2014-08-06 10:56:47 +0100

    clarify 007

--- arrays/exercise_007.php
+++ arrays/exercise_007.php
@@ -3,11 +3,14 @@
 $a1 = array(1,2,3,4,5,6,7,8,9,10);
 $a2 = array(6,7,8,9,10,11,12);
 
+var_dump(my_array_union($a1, $a2));
+
 /**
- * Give me the UNION of a1 and a2. That is, all the elements that are in A1 OR A2.
+ * Write a function that accepts two arrays and returns their UNION as an array. That is, all the elements that are in A1 OR A2.
  *
  * Avoid array_*() functions.
  */
 
-$a3 = ?;
+function my_array_union($array1, $array2) {
 
+}
