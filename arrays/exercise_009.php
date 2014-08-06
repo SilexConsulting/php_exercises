f9f9c26ef428895fca9d93e7714e657c5577c539
David Otton <david@otton.org>
2014-08-06 11:03:36 +0100

    clarify 009

--- arrays/exercise_009.php
+++ arrays/exercise_009.php
@@ -4,11 +4,16 @@ $a1 = array(1,2,3,4,5,6,7,8,9,10);
 $a2 = array(6,7,8,9,10,11,12);
 $a3 = array(2,4,6,8,10,12);
 
+var_dump(my_array_diff($a1, $a2));
+
 /**
- * Give me the DIFFERENCE of a1 and (a2,a3) That is, all the elements that are A1 BUT NOT IN A2 OR A3.
+ * Write a function that returns the DIFFERENCE between two arrays. That is, all the elements that are in A1 but NOT IN A2.
+ *
+ * Use that function, and possibly the functions from the previous two exercises, to build an array of all the elements that are in A1, but NOT IN (A2 OR A3)
  *
  * Avoid array_*() functions.
  */
 
-$a4 = ?;
+function my_array_diff($a1, $a2) {
 
+}
